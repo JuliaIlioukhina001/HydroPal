9948b2c681d3bf61e0897f5019330034772c5139
Vaishnav Mandru <106818193+Vaishnav469@users.noreply.github.com>
2024-05-19 05:37:04 -0400

    Update Profile.js

--- water-plant-front/src/pages/Profile.js
+++ water-plant-front/src/pages/Profile.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import profile from '../Assets/profile copy.png'
+import './css/profile.css'
+import { ShopContext } from '../context/context'
 
-const Profile = () => {
+const Profile = (props) => {
+  const {userInfo} = useContext(ShopContext)
+
   return (
-    <div>
-      <button  onClick={()=>{localStorage.removeItem('auth-token');window.location.replace("/");}}>LOGOUT</button>
-      
+    <div className='background'>
+      <div className='Header'>
+        <img src = {profile} alt = "" />
+        <p1>{userInfo.name}</p1>
+      </div>
+    <div className='email'>
+      Email: <div className='box-style'>
+        {userInfo.email}
+      </div>
     </div>
+    <div className='coins'>
+      Coins: <div className='coins-number'>
+        {userInfo.coins}
+      </div>
+    </div>
+
+    <div className='crypto-key'>
+      CryptoKey: <div className='box-style-1'>
+        {userInfo.cryptokey}
+      </div>
+    </div>
+    <button  onClick={()=>{localStorage.removeItem('auth-token');window.location.replace("/");}}>LOGOUT</button>
+      
+  </div>
   )
 }
 
